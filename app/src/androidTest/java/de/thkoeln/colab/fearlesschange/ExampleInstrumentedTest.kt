5a8294c7822ee6edd004c4fe0e3933c0f90e8faa
PBGyawali <38656630+PBGyawali@users.noreply.github.com>
2020-10-12 01:25:20 +0200

    Add files via upload

---
+++ app/src/androidTest/java/de/thkoeln/colab/fearlesschange/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package de.thkoeln.colab.fearlesschange
+
+//import androidx.test.platform.app.InstrumentationRegistry
+//import org.junit.Assert.assertEquals
+//import org.junit.Test
+
+/**
+ * Instrumented name, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleInstrumentedTest {
+//    @Test
+//    fun useAppContext() {
+//        val expected = "de.thkoeln.fherborn.fearlesschange"
+//        val actual = InstrumentationRegistry.getInstrumentation().context.packageName
+//        assertEquals(expected, actual)
+//    }
+}
